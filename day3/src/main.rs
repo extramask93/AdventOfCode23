6a23957f948deaa5c982e8d454e4b4c7605ea594
extramask93 <jozwiak.damian02@gmail.com>
2023-12-09 22:40:58 +0100

    Day3 part 2 solved, but ugly

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,10 @@
-use std::io::BufRead;
-
+use std::{
+    collections::{HashMap, HashSet},
+    io::BufRead,
+};
+fn isStar(c: u8) -> bool {
+    c == b'*'
+}
 fn isDigit(c: u8) -> bool {
     c >= b'0' && c <= b'9'
 }
@@ -24,6 +29,8 @@ fn main() {
     let reader = std::io::BufReader::new(fs);
 
     //prepare data
+    let mut stars: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    let mut nearby_stars: HashSet<(usize, usize)> = HashSet::new();
     let mut matrix = Vec::<Vec<u8>>::new();
     for mut line in reader.lines().filter_map(|elem| elem.ok()) {
         line = line.trim().to_string();
@@ -45,7 +52,15 @@ fn main() {
     {
         if is_surrounded == true {
             total += number;
+            nearby_stars.iter().for_each(|(xx, yy)| {
+                if stars.contains_key(&(*xx, *yy)) {
+                    stars.get_mut(&(*xx, *yy)).unwrap().push(number);
+                } else {
+                    stars.insert((*xx, *yy), vec![number]);
+                }
+            });
         }
+        nearby_stars.clear();
         number = 0;
         is_surrounded = false;
         for (c, y) in line.iter().skip(1).take(line.len() - 2).zip(1..line.len()) {
@@ -55,6 +70,9 @@ fn main() {
                 for (mx, my) in movements() {
                     let xx = (x as i32 - mx) as usize;
                     let yy = (y as i32 - my) as usize;
+                    if isStar(matrix[xx][yy]) {
+                        nearby_stars.insert((xx, yy));
+                    }
                     if !isDot(matrix[xx][yy]) && !isDigit(matrix[xx][yy]) {
                         is_surrounded = true;
                     }
@@ -62,16 +80,41 @@ fn main() {
             } else {
                 if is_surrounded == true {
                     total += number;
+                    nearby_stars.iter().for_each(|(xx, yy)| {
+                        if stars.contains_key(&(*xx, *yy)) {
+                            stars.get_mut(&(*xx, *yy)).unwrap().push(number);
+                        } else {
+                            stars.insert((*xx, *yy), vec![number]);
+                        }
+                    });
                 }
                 number = 0;
                 is_surrounded = false;
+                nearby_stars.clear();
             }
         }
     }
     if is_surrounded == true {
+        nearby_stars.iter().for_each(|(xx, yy)| {
+            if stars.contains_key(&(*xx, *yy)) {
+                stars.get_mut(&(*xx, *yy)).unwrap().push(number);
+            } else {
+                stars.insert((*xx, *yy), vec![number]);
+            }
+        });
         total += number;
     }
     println!("{}", total);
+    stars.iter().for_each(|(key,val)| {
+        print!("({},{}) = ",key.0,key.1);
+        val.iter().for_each(|v|{print!("{},",v)});
+        println!("");
+    });
+    println!("{}", stars.iter().filter(|(key,val)| {
+        val.len() == 2
+    }).fold(0, |acc, (key,val)| {
+        acc + val.iter().fold(1, |sum, cv| {sum*cv})
+    }));
 }
 
 mod test {}
